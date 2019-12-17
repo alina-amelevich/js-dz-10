aa7c947e2e335bb850b6a65ad9e2798ecc0f0bf7
Alina <alina@Apples-MacBook-Pro.local>
2019-12-17 03:52:38 +0300

    Done!

--- mood.js
+++ mood.js
@@ -1,15 +1,22 @@
 "use strict";
 
 function randomDiap(n, m) {
-        return Math.floor(Math.random() * (m - n +1 )) + n;
+        return Math.floor(Math.random() * (m - n + 1 )) + n;
 }
 
 function mood(colorsCount) {
     var colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+    var colorNumbers = {};
 
     console.log('цветов: ' + colorsCount);
     for (var i = 1; i <= colorsCount; i++) {
         var n = randomDiap(1, 7);
+        if (n in colorNumbers) {
+            while (n in colorNumbers) {
+                n = randomDiap(1, 7);
+            }
+        }
+        colorNumbers[n] = true;
         var colorName = colors[n];
         console.log(colorName);
     }
